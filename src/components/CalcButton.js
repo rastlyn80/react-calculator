d9a131a82d2e3a3d3c229bd58450e7d3408dfa65
Gabriel Baldock <gabrielbaldock@gmail.com>
2022-09-24 13:31:07 +0100

    Implemented button clicks and wired it to Redux state

--- src/components/CalcButton.js
+++ src/components/CalcButton.js
@@ -1,11 +1,18 @@
 import React from "react";
+import { connect } from "react-redux";
+import { addDigit } from "../actions";
 
 class CalcButton extends React.Component {
+  processClick = () => {
+    this.props.addDigit(this.props.value);
+  };
+
   render() {
     return (
       <div
         className={`calc-button ${this.props.buttonStyle}`}
         id={this.props.id}
+        onClick={this.processClick}
       >
         {this.props.value}
       </div>
@@ -13,4 +20,4 @@ class CalcButton extends React.Component {
   }
 }
 
-export default CalcButton;
+export default connect(null, { addDigit })(CalcButton);
